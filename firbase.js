d10c213049c69b43891a86fe68c2934862405b9c
casp-e-r <aswinkarooth@gmail.com>
2022-03-07 23:04:23 +0530

    firebase config changed,new google api

--- firbase.js
+++ firbase.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase'
 const firebaseConfig = {
-    apiKey: "AIzaSyDslY79pYzImZnbMmtWRvPodzUBxMvU67I",
-    authDomain: "amzn-nxt.firebaseapp.com",
-    projectId: "amzn-nxt",
-    storageBucket: "amzn-nxt.appspot.com",
-    messagingSenderId: "653575831701",
-    appId: "1:653575831701:web:b27d4d5fa79037dc01db9c"
-  };
+
+  apiKey: "AIzaSyB2aU4fuYxtJ7TidZ626aw0o21xDNnkP5Q",
+
+  authDomain: "amzn-83928.firebaseapp.com",
+
+  projectId: "amzn-83928",
+
+  storageBucket: "amzn-83928.appspot.com",
+
+  messagingSenderId: "1056045299051",
+
+  appId: "1:1056045299051:web:2ce2b66965ccb06138882e"
+}
